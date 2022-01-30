acb63f9cee3b5834bca7a73cf1c03a78bc9a6305
asadpro <asadjani202@gmail.com>
2022-01-30 12:26:49 +0500

    This is the initial code of show 1

---
+++ android/app/src/main/kotlin/com/example/the_boring_show_day_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.the_boring_show_day_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/article.dart
@@ -0,0 +1,96 @@
+class Article {
+  final String text;
+  final String domain;
+  final String by;
+  final String age;
+  final int score;
+  final int commentsCount;
+
+  const Article(
+      {required this.text,
+      required this.domain,
+      required this.by,
+      required this.age,
+      required this.score,
+      required this.commentsCount});
+}
+
+final List<Article> articles = [
+  Article(
+      text: 'Hamza Mehmood Title',
+      domain: 'Wiley.com',
+      by: 'ZDW',
+      age: '3 Hours',
+      score: 177,
+      commentsCount: 63),
+  Article(
+      text: 'Sajad Ahmad Title',
+      domain: 'Wiley.com',
+      by: 'ZDW',
+      age: '3 Hours',
+      score: 177,
+      commentsCount: 23),
+  Article(
+      text: 'Muhib khan Title',
+      domain: 'Wiley.com',
+      by: 'ZDW',
+      age: '3 Hours',
+      score: 177,
+      commentsCount: 43),
+  Article(
+      text: 'Asad Pro Title',
+      domain: 'Wiley.com',
+      by: 'ZDW',
+      age: '3 Hours',
+      score: 177,
+      commentsCount: 53),
+  Article(
+      text: 'Saqib Title',
+      domain: 'Wiley.com',
+      by: 'ZDW',
+      age: '3 Hours',
+      score: 177,
+      commentsCount: 83),
+  Article(
+      text: 'Muhib khan Title',
+      domain: 'Wiley.com',
+      by: 'ZDW',
+      age: '3 Hours',
+      score: 177,
+      commentsCount: 43),
+  Article(
+      text: 'Asad Pro Title',
+      domain: 'Wiley.com',
+      by: 'ZDW',
+      age: '3 Hours',
+      score: 177,
+      commentsCount: 53),
+  Article(
+      text: 'Saqib Title',
+      domain: 'Wiley.com',
+      by: 'ZDW',
+      age: '3 Hours',
+      score: 177,
+      commentsCount: 83),
+  Article(
+      text: 'Muhib khan Title',
+      domain: 'Wiley.com',
+      by: 'ZDW',
+      age: '3 Hours',
+      score: 177,
+      commentsCount: 43),
+  Article(
+      text: 'Asad Pro Title',
+      domain: 'Wiley.com',
+      by: 'ZDW',
+      age: '3 Hours',
+      score: 177,
+      commentsCount: 53),
+  Article(
+      text: 'Saqib Title',
+      domain: 'Wiley.com',
+      by: 'ZDW',
+      age: '3 Hours',
+      score: 177,
+      commentsCount: 83),
+];
