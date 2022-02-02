fb701d2c90f7549a2a74eaaa8f598eef6cb9d9b5
asadpro <asadjani202@gmail.com>
2022-02-02 14:31:48 +0500

    delete this commit

--- lib/main.dart
+++ lib/main.dart
@@ -63,10 +63,7 @@ class _MyAppState extends State<MyApp> {
                 article.commentsCount.toString(),
                 textScaleFactor: 2.0,
               ),
-              Text(
-                article.commentsCount.toString(),
-                textScaleFactor: 2.0,
-              ),
+             
               IconButton(
                   onPressed: () {
                     launch('https://flutterawesome.com/');
